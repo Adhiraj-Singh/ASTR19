03a75f837ada94428cd33c72244ab44c0a3f78a3
Adhiraj Singh <86346441+Adhiraj-Singh@users.noreply.github.com>
2022-04-23 03:33:59 +0530

    Add files via upload

---
+++ session2.py
@@ -0,0 +1,25 @@
+def main():
+    print("Sum of two floating point numbers->2.34-5.89")
+    print(2.34+5.89)
+    print("Data type->")
+    print(type(2.34+5.89))
+    print("\n")
+    
+    
+    print("Difference between two integers-> 94-34")
+    print(94-34)
+    print("Data type->")
+    print(type(94-34))
+    print("\n")
+    
+    print("Product of a floating point number and an integer->534.233*443")
+    print(534.233*443)
+    print("Data type->")
+    print(type(534.233*443))
+    
+    
+
+if __name__=="__main__":
+    main()
+
+
