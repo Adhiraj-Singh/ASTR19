648e8d4d89d48be5363b3ff27e65c63eea02d932
Adhiraj Singh <86346441+Adhiraj-Singh@users.noreply.github.com>
2022-04-23 03:22:09 +0530

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,10 @@
+def main():
+    print("Adhiraj Singh")
+    print("He/Him/His")
+    print("Step up 3 had the best visual effects and choerography")
+    print("Sushi is expensive but totally worth it")
+
+if __name__=="__main__":
+    main()
+
+
